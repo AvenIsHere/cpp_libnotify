9591116e8ea442a8fd69c52b888b00464c0fd228
Aven <a@avenf.com>
2026-03-19 23:48:42 +0000

    fbfh

--- library.h
+++ library.h
@@ -1,29 +1,49 @@
 #ifndef CPP_LIBNOTIFY_LIBRARY_H
 #define CPP_LIBNOTIFY_LIBRARY_H
 
-#include <chrono>
+#include <functional>
 #include <string>
-#include <libnotify/notification.h>
+#include <memory>
+#include <glibmm>
 
-struct NotificationDeleter {
-    void operator()(NotifyNotification* n) {
-        if (n) g_object_unref(n);
-    }
+extern "C" {
+    typedef struct _NotifyNotification NotifyNotification; // NOLINT(bugprone-reserved-identifier)
+    typedef struct _GVariant GVariant; // NOLINT(bugprone-reserved-identifier)
+}
+
+enum class Urgency {
+    LOW,
+    NORMAL,
+    CRITICAL,
 };
 
 class Notification {
-    std::unique_ptr<NotifyNotification, NotificationDeleter> m_notification;
+    struct Notification_Deleter { void operator()(NotifyNotification* n) const; };
+    struct Variant_Deleter { void operator()(GVariant* v) const; };
+
+    using notify_notification = std::unique_ptr<NotifyNotification, Notification_Deleter>;
+    using g_variant = std::unique_ptr<GVariant, Variant_Deleter>;
+
+    notify_notification m_notification;
 public:
     // constructors
-    explicit Notification(const std::string& given_summary = "",
-                          const std::string& given_body = "",
-                          const std::string& given_icon = "");
+    explicit Notification(const std::string& given_summary,
+                          const std::string& given_body,
+                          const std::string& given_icon);
     explicit Notification(NotifyNotification* given_notification);
 
     // setters (they return references themselves to make chaining possible)
     Notification& set_notification(NotifyNotification*& given_notification);
     Notification& set_summary(const std::string& given_summary);
     Notification& set_body(const std::string& given_body);
+    Notification& set_urgency(Urgency given_urgency);
+    Notification& set_timeout(int given_timeout);
+    Notification& set_hint(const std::string& given_key);
+
+    using ActionCallback = std::function<void(Notification&, const std::string&)>;
+    Notification& add_action(const std::string& given_id,
+                             const std::string& given_label,
+                             ActionCallback given_callback);
 
     // getters
     [[nodiscard]] NotifyNotification* get_NotifyNotification() const;
@@ -36,7 +56,7 @@ public:
     static bool de_init();
 
     // member functions
-    int send();
+    bool show();
 };
 
 #endif // CPP_LIBNOTIFY_LIBRARY_H
